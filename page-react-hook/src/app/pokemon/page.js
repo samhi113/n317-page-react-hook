33db33ffce622f8facc57464f57cfc686f4d4036
samhigg <samhigg@iu.edu>
2024-09-18 10:48:34 -0400

    fix plz

---
+++ page-react-hook/src/app/pokemon/page.js
@@ -0,0 +1,11 @@
+export default function Pokemon() {
+  return (
+    <main>
+      <h1>Pokemon Page</h1>
+      <div>
+        <input type="search" id="search" name="search"/>
+        <input type="button" value="Search"/>
+      </div>
+    </main>
+  );
+}
--- pages-and-react-hooks-main/page-react-hook/src/app/pokemon/page.js
+++
@@ -1,7 +0,0 @@
-export default function Pokemon() {
-  return (
-    <main>
-      <h1>Pokemon Page</h1>
-    </main>
-  );
-}
